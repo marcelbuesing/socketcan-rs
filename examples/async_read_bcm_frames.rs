36d36a757704428b2db8a5cce7758b85cd2ed4b7
marcelbuesing <buesing.marcel@googlemail.com>
2018-05-17 12:47:06 +0200

    Add async filtering example

---
+++ examples/async_read_bcm_frames.rs
@@ -0,0 +1,21 @@
+extern crate futures;
+extern crate socketcan;
+extern crate tokio;
+use futures::stream::Stream;
+use socketcan::bcm::async::*;
+use socketcan::FrameFlags;
+use std::time;
+
+fn main() {
+    let socket = CanBCMSocket::open_nb("vcan0").unwrap();
+    let ival = time::Duration::from_millis(0);
+    let f = socket
+        .filter_id_incoming_frames(0x123, ival, ival, FrameFlags::empty())
+        .unwrap()
+        .map_err(|err| eprintln!("IO error {:?}", err))
+        .for_each(|frame| {
+            println!("Frame {:?}", frame);
+            Ok(())
+        });
+    tokio::run(f);
+}
